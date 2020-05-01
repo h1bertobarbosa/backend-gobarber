a9c53ad7cbaf79f302579dd6bc9735a7da8a9eb6
Humberto Barbosa <humberto.obarbosa@gmail.com>
2020-04-30 23:18:46 -0300

    criptografia senha user

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
@@ -19,10 +20,12 @@ export default class CreateUserService {
       throw new Error('Email address already used.');
     }
 
+    const hashedPass = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPass,
     });
 
     return usersRepository.save(user);
