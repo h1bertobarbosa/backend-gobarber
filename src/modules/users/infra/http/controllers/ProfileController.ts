d5286386cfc6907017cac75d109d9f4e89da91b9
Humberto Barbosa <humberto.obarbosa@gmail.com>
2020-08-21 00:28:31 -0300

    correcao response json profile

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
+import { classToClass } from 'class-transformer';
 
 export default class ProfileController {
   public async show(request: Request, response: Response): Promise<Response> {
@@ -26,6 +27,6 @@ export default class ProfileController {
       password,
     });
 
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
